e12cffe04d567c98e42260cbe0d270f9f5af1150
Enlitem <eprimazov@yandex.ru>
2023-07-16 15:30:14 +0300

    Стилизация

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="container">
     <TabView>
-      <TabPanel header="Таблица">
+      <TabPanel>
+        <template #header>
+          <i class="pi pi-list table-icon"></i>
+          <span>Таблица</span>
+        </template>
         <Table></Table>
       </TabPanel>
-      <TabPanel header="Карточки">
+      <TabPanel>
+        <template #header>
+          <i class="pi pi-th-large table-icon "></i>
+          <span>Карточки</span>
+        </template>
         <div>Cards</div>
       </TabPanel>
     </TabView>
@@ -23,8 +31,11 @@ import Table from './components/TableComponent.vue';
   min-height: 100wh;
 }
 
-:deep(.p-tabview .p-tabview-panels){
+:deep(.p-tabview .p-tabview-panels) {
   padding: 0;
 }
 
+.table-icon {
+  margin-right: 1rem;
+}
 </style>
